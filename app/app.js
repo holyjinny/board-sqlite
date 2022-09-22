a37be7431e502e5fdb15d23c8f8f1c4b518cb98c
holyjinny <koreavzz@naver.com>
2022-09-22 18:24:08 +0900

    log: winston & morgan

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
 
 const app = express();
+dotenv.config();
 
 // 라우팅
 const home = require("./src/routes/home");
---
+++ app/src/config/logger.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const { createLogger, transports, format } = require("winston");
+const { combine, timestamp, printf, label, colorize, simple } = format;
+
+const printFormat = printf(({ timestamp, label, level, message }) => {
+    return `${timestamp} [${label}] ${level} : ${message} !!`
+});
+
+const printLogFormat = {
+    file: combine(
+        label({
+            label: "노드 프로젝트",
+        }),
+        timestamp({
+            format: "YYYY-MM-DD HH:mm:dd",
+        }),
+        printFormat
+    ),
+    console: combine(
+        colorize(),
+        simple()
+    )
+};
+
+const opts = {
+    file: new transports.File({
+        filename: "winston.log",
+        dirname: "./log",
+        level: "info",
+        format: printLogFormat.file,
+    }),
+    console: new transports.Console({
+        level: "info",
+        format: printLogFormat.console,
+    })
+}
+
+const logger = createLogger({
+    transports: [opts.file],
+});
+
+if (process.env.NODE_ENV !== "production") {
+    logger.add(opts.console);
+};
+
+module.exports = logger;
